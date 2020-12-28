c0da6ea6075f20d14f425eac753386700aede0fc
Javier <neiker@gmail.com>
2020-12-28 21:28:00 +0100

    Fix linter

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: "react-native-wcandillon",
+  rules: {
+    "no-console": "error",
+    "comma-dangle": 0, // https://github.com/facebook/react-native/commit/55e974d9faeae66d6ec5030ff23fa3e80e2097aa
+  },
+};
