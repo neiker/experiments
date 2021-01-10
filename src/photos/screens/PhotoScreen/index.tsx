61d55be2b49c26328eb82376f949f8e098beff4d
Javier <neiker@gmail.com>
2021-01-10 18:24:07 +0100

    PhotoScreen: Replace ScrollView with FlatList and add animation to photo title

---
+++ src/photos/screens/PhotoScreen/index.tsx
@@ -0,0 +1,51 @@
+import { RouteProp } from "@react-navigation/native";
+import React from "react";
+import { useWindowDimensions } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
+
+import { Photo, PhotosStackProps } from "../../types";
+
+import { PhotoItem } from "./PhotoItem";
+
+interface PhotoScreenProps {
+  route: RouteProp<PhotosStackProps, "Photo">;
+}
+
+export function PhotoScreen({ route }: PhotoScreenProps) {
+  const { photos, photoId } = route.params;
+  const { width: windowWidth } = useWindowDimensions();
+
+  const scrollViewRef = React.useRef<FlatList<Photo>>(null);
+
+  React.useEffect(() => {
+    if (scrollViewRef.current) {
+      const photo = photos.find((p) => p.id === photoId);
+
+      if (photo) {
+        scrollViewRef.current.scrollToItem({
+          item: photo,
+          animated: false,
+        });
+      }
+    }
+  }, [photos, photoId, windowWidth]);
+
+  return (
+    <FlatList
+      horizontal
+      snapToInterval={windowWidth}
+      decelerationRate="fast"
+      ref={scrollViewRef}
+      data={photos}
+      keyExtractor={(photo) => `${photo.id}`}
+      renderItem={({ item: photo }) => (
+        <PhotoItem key={photo.id} photo={photo} width={windowWidth} />
+      )}
+      getItemLayout={(_, index) => ({
+        length: windowWidth,
+        offset: windowWidth * index,
+        index,
+      })}
+    />
+  );
+}
